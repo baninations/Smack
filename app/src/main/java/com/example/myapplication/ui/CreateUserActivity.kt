bdcd459a7eacdf090a4fd4dc37fc6fcb20bc7c87
Alban <baninations@gmail.com>
2020-07-21 19:28:18 +0200

    Lesson 05, pre-Api and web sockets

--- app/src/main/java/com/example/myapplication/ui/CreateUserActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/CreateUserActivity.kt
@@ -1,20 +1,50 @@
 package com.example.myapplication.ui
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.myapplication.R
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5. 1]"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
     }
 
     fun generateUserAvatarClicked(view: View){
+        val random = Random()
+        val color = random.nextInt(2)
+        val avatar = random.nextInt(28)
 
+        if(color == 0){
+            userAvatar = "light$avatar"
+        }else{
+                userAvatar = "dark$avatar"
+        }
+        val resourceID = resources.getIdentifier(userAvatar, "drawable", packageName)
+        createAvatarImageView.setImageResource(resourceID)
     }
     fun generateColorClicked(view: View){
+        val random = Random()
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
+
+        createAvatarImageView.setBackgroundColor(Color.rgb(r,g,b))
+
+        val savedR = r.toDouble() / 255
+        val savedG = g.toDouble() / 255
+        val savedB = b.toDouble() / 255
+
+        avatarColor = "[$savedR, $savedG, $savedB, 1]"
+        println(avatarColor)
 
     }
     fun createUserButtonClicked(view: View){
